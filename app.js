ec9103bd36acb7edf5498afe5934c750d4b1ff62
NASA_Mark <76894588+Atharane@users.noreply.github.com>
2022-08-09 15:48:26 +0530

    refactor code

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-let wrapper = document.getElementById('wrapper');   
+let wrapper = document.getElementById('wrapper');
 
-grid = []
-
-sudukustr = "2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3";
+// sudukustr = "2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3";
+    
+sudukuarr = [
+  [2, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 6, 2, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 7, 0],
+  [0, 0, 6, 0, 0, 8, 0, 0, 0],
+  [3, 0, 0, 0, 9, 0, 0, 0, 7],
+  [0, 0, 0, 6, 0, 0, 4, 0, 0],
+  [0, 4, 0, 0, 0, 0, 8, 0, 0],
+  [0, 0, 5, 2, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 3],
+];
 
 function renderCells() {
-    for (let i = 0; i < 81; i++) {
-        console.log(i)
-        let box = document.createElement("div")
-        box.classList.add('box')    
-        box.id = i
-        box.textContent = sudukustr[i]
-        wrapper.appendChild(box)
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        let box = document.createElement("div");
+        box.classList.add("square");
+        box.textContent = sudukuarr[row][col];
+        wrapper.appendChild(box);
+      }
     }
 }
 
