e64481fc73b49f2539960f3f4c9dc40e0588665a
NASA_Mark <76894588+Atharane@users.noreply.github.com>
2022-08-13 12:44:23 +0530

    add solve puzzle logic

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let wrapper = document.getElementById('wrapper');
+let puzzleEl = document.getElementById("puzzle");
 
 // sudukustr = "2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3";
-    
-sudukuarr = [
+
+sudokuArr = [
   [2, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 6, 2, 0, 0],
   [0, 0, 1, 0, 0, 0, 0, 7, 0],
@@ -14,15 +14,73 @@ sudukuarr = [
   [0, 0, 0, 0, 0, 0, 0, 0, 3],
 ];
 
-function renderCells() {
-    for (let row = 0; row < 9; row++) {
-      for (let col = 0; col < 9; col++) {
-        let box = document.createElement("div");
-        box.classList.add("square");
-        box.textContent = sudukuarr[row][col];
-        wrapper.appendChild(box);
-      }
+function renderCells(arr) {
+  // remove all children from puzzleEl
+  while (puzzleEl.firstChild) {
+    puzzleEl.removeChild(puzzleEl.firstChild);
+  }
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      let box = document.createElement("div");
+      box.classList.add("square");
+
+      // set as number or set as "" if 0
+      if (arr[row][col]) box.textContent = arr[row][col];
+      else box.textContent = "";
+
+      puzzleEl.appendChild(box);
     }
+  }
 }
 
-renderCells()
+function isValid(row, col, value) {
+  for (let i = 0; i < 9; i++) {
+    if (sudokuArr[row][i] == value || sudokuArr[i][col] == value) return false;
+  }
+
+  // check if duplicate element exists in box
+  let boxRow = Math.floor(row / 3) * 3;
+  let boxCol = Math.floor(col / 3) * 3;
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (sudokuArr[boxRow + i][boxCol + j] == value) return false;
+    }
+  }
+
+  return true;
+}
+
+function sudokuSolver() {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (row == 8 && col == 8) return true;
+      if (sudokuArr[row][col]) continue;
+
+      for (let value = 1; value <= 9; value++) {
+        if (isValid(row, col, value)) {
+          // console.log("placing " + value + " at " + row + " " + col);
+          if (row == 0 && col == 5) {
+            console.log("placing " + value + " at " + row + " " + col);
+          }
+          sudokuArr[row][col] = value;
+
+          if (sudokuSolver(sudokuArr)) {
+            return true;
+          }
+          sudokuArr[row][col] = 0;
+        }
+      }
+      console.log("arr [" + row + "][" + col + "] = " + sudokuArr[row][col]);
+      return false;
+    }
+  }
+}
+
+document.getElementById("solve-button").addEventListener("click", function (e) {
+  sudokuSolver();
+  renderCells(sudokuArr);
+});
+
+renderCells(sudokuArr);
